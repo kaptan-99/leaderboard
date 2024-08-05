bd2db85b1eed0f60631ef5d38d7141a298052e68
Gurdeep <gurdeep.singh@auw.co.in>
2024-08-05 23:08:44 +0530

    adding subNavbar buttons

--- src/app/components/subNavbar.jsx
+++ src/app/components/subNavbar.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const SubNavbar = () => {
@@ -6,8 +7,11 @@ const SubNavbar = () => {
         <div class="max-w-screen-xl px-4 py-3 mx-auto">
             <div class="flex items-center">
                 <ul class="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+                <li>
+                        <Link href="/dashboard" class="text-gray-900 dark:text-white hover:underline" aria-current="dashboard">Home</Link>
+                    </li>
                     <li>
-                        <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Social Media</a>
+                        <Link href="/dashboard/sm" class="text-gray-900 dark:text-white hover:underline" aria-current="sm">Social Media</Link>
                     </li>
                    
                 </ul>
